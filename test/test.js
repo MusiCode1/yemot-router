308b91eb85a6feadde488f3867e095f848c94f2a
Avi Britman <aybritman@gmail.com>
2019-10-22 13:47:39 +0300

    The test has been fixed

--- test/test.js
+++ test/test.js
@@ -1,17 +1,30 @@
-var assert = require("assert");
+const assert = require("assert");
 
 describe("testeing in yemot-router", () => {
 
-	describe("testing ");
+	describe("testing response_functions", () => {
 
-});
+		const response_functions = new (require("../lib/response_functions"));
 
+		describe("testeing make_read_response function", () => {
 
+			const make_read_response = response_functions.make_read_response;
 
-describe("Array", function () {
-	describe("#indexOf()", function () {
-		it("should return -1 when the value is not present", function () {
-			assert.equal([1, 2, 3].indexOf(4), -1);
+			let massage = [];
+			let options = {};
+			let r = "";
+
+			it("", () => {
+
+				massage = [{ type: "text", data: "hay, is text!" }];
+
+				r = make_read_response(massage, "tap", options);
+
+				assert.equal(r[0], "read=t-hay, is text!=,val_1,no,*,1,7,Number,false,true,,,,");
+				assert.equal(r[1], "val_1");
+			});
+
 		});
 	});
+
 });
