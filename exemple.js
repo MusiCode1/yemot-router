89c528bb6f9dd570bd49c8d6428b91273b5642a2
ShlomoCode <shlomomail16@gmail.com>
2022-05-26 23:58:50 +0300

    עריכת תיעוד

--- exemple.js
+++ exemple.js
@@ -9,8 +9,8 @@ const y = Yemot_router();
 
 y.add_fn("/", async (call) => {
 
-	let massage = [{ type: "text", data: "היי, תקיש 10" }];
-	let r = await call.read(massage)
+	let massage = [{ type: "text", data: "שלום, אנא הקש את שמך המלא" }];
+	let r = await call.read(massage, "tap", { play_ok_mode: "HebrewKeyboard" })
 		.catch(error => {
 			if (error.name === "Hangup_error")
 				console.log(error.call.phone, "hangup");
@@ -21,7 +21,7 @@ y.add_fn("/", async (call) => {
 	console.log(r);
 
 	massage = [
-		{ type: "text", data: "הקשת " + r },
+		{ type: "text", data: "שלום " + r },
 		{ type: "text", data: "אנא הקלט את הרחוב בו אתה גר" }
 	];
 	r = await call.read(massage, "record");
